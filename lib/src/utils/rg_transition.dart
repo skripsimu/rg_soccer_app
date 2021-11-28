0254476f8423ae7919c18134bd61d5d9c4541c7b
tegar <tegar.nugroho@danafix.id>
2021-11-28 22:10:30 +0700

    change page transition

--- lib/src/utils/rg_transition.dart
+++ lib/src/utils/rg_transition.dart
@@ -3,43 +3,30 @@ import 'package:flutter/material.dart';
 class RGTransition extends PageRouteBuilder {
   final Widget widget;
   RGTransition({required this.widget})
-      : super(pageBuilder: (BuildContext context, Animation<double> animation,
-            Animation<double> secondaryAnimation) {
-          return widget;
-        }, transitionsBuilder: (BuildContext context,
+      : super(
+          pageBuilder: (
+            BuildContext context,
             Animation<double> animation,
             Animation<double> secondaryAnimation,
-            Widget child) {
-          return new ScaleTransition(
-            scale: new Tween<double>(
+          ) =>
+              widget,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              ScaleTransition(
+            scale: Tween<double>(
               begin: 0.0,
               end: 1.0,
             ).animate(
               CurvedAnimation(
                 parent: animation,
-                curve: Interval(
-                  0.00,
-                  0.50,
-                  curve: Curves.bounceIn,
-                ),
+                curve: Curves.fastOutSlowIn,
               ),
             ),
-            child: ScaleTransition(
-              scale: Tween<double>(
-                begin: 1.5,
-                end: 1.0,
-              ).animate(
-                CurvedAnimation(
-                  parent: animation,
-                  curve: Interval(
-                    0.50,
-                    1.00,
-                    curve: Curves.easeIn,
-                  ),
-                ),
-              ),
-              child: child,
-            ),
-          );
-        });
+            child: child,
+          ),
+        );
 }
